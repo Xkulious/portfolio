7c06578044f916feb2e32657bb5271223b26925a
boenliu <boenliu@outlook.com>
2022-03-18 16:15:49 -0400

    --- Auto-Git Commit ---

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 const backgroundImage = document.querySelector(".MeatnPotatos");
-const 
+const resume = document.querySelector("#resume");
+const contact = document.querySelector("#contact");
+const projects = document.querySelector("#projects");
+
+resume.addEventListener("mouseenter", () => {
+    backgroundImage.style.backgroundImage = "url('images/resume.jpg')";
+    backgroundImage.style.transition = ""
+});
+
+resume.addEventListener("mouseleave", () => {
+    backgroundImage.style.backgroundImage = "";
+
+});
