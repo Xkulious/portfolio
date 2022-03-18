be103a986403ea46a1db85b20b864e8c5037e5b0
boenliu <boenliu@outlook.com>
2022-03-18 19:28:54 -0400

    --- Auto-Git Commit ---

---
+++ js/project.js
@@ -0,0 +1,9 @@
+const background = document.querySelector(".background");
+
+const pDesc = document.querySelector(".pixelated");
+const mDesc = document.querySelector(".mountains");
+
+const pButton = document.querySelector(".pbutton");
+const mButton = document.querySelector(".mbutton");
+
+pButton.addEventListener("")
