ffc18cb843673ce9f95ad61b1bacea51a4f4002f
boenliu <boenliu@outlook.com>
2022-03-18 20:13:18 -0400

    fdsa

--- js/project.js
+++ js/project.js
@@ -3,12 +3,16 @@ const mDesc = document.querySelector(".mountains");
 const pButton = document.querySelector(".pbutton");
 const mButton = document.querySelector(".mbutton");
 
+document.body.style.backgroundImage = "url('images/white.png')";
+
 pButton.addEventListener("click", () => {
-    pDesc.style.opacity = "1"
+    pDesc.style.opacity = "1";
+    mDesc.style.opacity = "0";
     document.body.style.backgroundImage = "url('images/pixelated.png')";
 });
 
 mButton.addEventListener("click", () => {
-    mDesc.style.opacity = "1"
+    mDesc.style.opacity = "1";
+    pDesc.style.opacity = "0";
     document.body.style.backgroundImage = "url('images/relativeexpression.png')";
 });
